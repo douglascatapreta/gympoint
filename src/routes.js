b9ca88288141ec03a6d6cc5b6c71536aea5eaf94
Douglas Cata Preta <douglascatapreta@gmail.com>
2019-11-07 22:38:08 -0300

    Criação do Middleware de autenticação.

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,15 @@
 import { Router } from 'express';
 import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.post('/users', (req, res) => {
+  return res.json({ ok: true });
+});
 
 export default routes;
