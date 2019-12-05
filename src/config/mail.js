7c6fe114c535811dc0c39b70c1c814ba6fa995d5
Douglas Cata Preta <douglascatapreta@gmail.com>
2019-12-04 23:35:02 -0300

    Criação do arquivo com as configurações do serviço de envio de emails.

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'b497da7aa716d4',
+    pass: '64e5ba21c667c2',
+  },
+  default: {
+    from: 'Equipe GymPoint <noreply@gympoint.com>',
+  },
+};
